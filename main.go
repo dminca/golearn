709e2fbc522728ea92140f99d8639f82d468b77c
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2024-10-26 20:50:16 +0200

    feat: retrieve body and decode
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
 
 type HttpDecoder interface {
@@ -15,27 +17,46 @@ type HttpDecoder interface {
 const DataLink string = "https://data.gov.ro/dataset/fbeab450-4092-40c7-b0ef-ce0222e6c17b/resource/3cd79e1f-6b75-4b6f-bf32-903e2b6fad51/download/wifi_cluj.json"
 
 func main() {
-	data, err := RetrieveRequest(DataLink)
+	httpReq := HttpGetter(DataLink)
+	defer httpReq.Body.Close()
+	data, err := RetrieveRequest(httpReq)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(string(data))
-}
 
-// RetrieveRequest dumps the HTTP Request
-func RetrieveRequest(link string) ([]byte, error) {
-	req, err := http.NewRequest(http.MethodGet, link, nil)
+	dataReturned, err := RetrieveBody(httpReq)
 	if err != nil {
 		log.Fatal(err)
 	}
-	reqDump, err := httputil.DumpRequest(req, false)
+	fmt.Println(string(dataReturned))
+}
+
+// RetrieveRequest dumps the HTTP Request
+func RetrieveRequest(req *http.Response) ([]byte, error) {
+	reqDump, err := httputil.DumpResponse(req, false)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return reqDump, nil
 }
 
+func HttpGetter(link string) *http.Response {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	req, err := client.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return req
+}
+
 // RetrieveBody dumps the body of an HTTP request
-func RetrieveBody() {
-	panic("not implemented")
+func RetrieveBody(req *http.Response) ([]byte, error) {
+	respDump, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalf("Failed to read body: %v", err)
+	}
+	return respDump, nil
 }
