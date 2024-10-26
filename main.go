2ae3205d745f5250598b3cb1fbd0da3570242bce
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2024-10-26 23:34:53 +0200

    feat: write arguments passed to file
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("No arguments provided")
+	}
+
+	composeContent := strings.Join(args, " \\\n")
+	if err := os.WriteFile("outfile.txt", []byte(composeContent), 0644); err != nil {
+		log.Fatalf("Error writing args to file: %v", err)
+	}
 }
