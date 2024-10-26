b6ffa4e3dd217d308579c11d2bcb49c62faa60a7
Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>
2024-10-26 14:31:12 +0200

    fix: call some URL
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Mincă <zealous_sterner.0e@icloud.com>

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+type HttpDecoder interface {
+	RetrieveRequestOrBody(string) ([]byte, error)
+}
+
+const DataLink string = "https://data.gov.ro/dataset/fbeab450-4092-40c7-b0ef-ce0222e6c17b/resource/3cd79e1f-6b75-4b6f-bf32-903e2b6fad51/download/wifi_cluj.json"
+
 func main() {
+	data, err := RetrieveRequest(DataLink)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+}
+
+func RetrieveRequest(link string) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, link, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reqDump, err := httputil.DumpRequest(req, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return reqDump, nil
+}
+
+func RetrieveBody() {
 	panic("not implemented")
 }
